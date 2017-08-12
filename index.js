cc9acbaafa9aaa6603cacd304693f2dfa87bb7c7
Magnus Jonsson <fot@fot.nu>
2017-08-12 23:38:34 +0200

    Added a save button that using cookies saved the config.

--- index.js
+++ index.js
@@ -15,4 +15,49 @@ $(function() {
 		+ "/" + $("select[name=fwpm]").val();
 		return false;
 	});
+	$("#acceptButton").click(function() {
+		Cookies.set('accept-cookies',"yes" , { expires: 30, path: '' });
+		$("#save").click();
+		setTimeout(function() {
+			document.location = "#close";
+		},100);
+	});
+	$("#save").click(function() {
+		if(Cookies.get("accept-cookies") !== undefined) {
+			Cookies.set('count', $("select[name=count]").val() , { expires: 30, path: '' });
+			Cookies.set('pitch', $("select[name=pitch]").val() , { expires: 30, path: '' });
+			Cookies.set('wpm', $("select[name=wpm]").val() , { expires: 30, path: '' });
+			Cookies.set('fwpm', $("select[name=fwpm]").val() , { expires: 30, path: '' });
+                        $(this).animate({ backgroundColor: "green" }, 250, function() {
+                                $(this).animate({ backgroundColor: "#ddd" }, 250);
+                        });
+
+		} else {
+			setTimeout(function() {
+                                document.location = "#cookieDialog";
+                        },100);
+		}
+	});
+	if(Cookies.get("accept-cookies") !== undefined) {
+		let count = Cookies.get('count');
+		if(count === undefined) {
+			count = 10;
+		}
+		let pitch = Cookies.get('pitch');
+		if(pitch === undefined) {
+			pitch = 550;
+		}
+		let wpm = Cookies.get('wpm');
+		if(wpm === undefined) {
+			wpm = 20;
+		}
+		let fwpm = Cookies.get('fwpm');
+		if(fwpm === undefined) {
+			fwpm = 10;
+		}
+		$("select[name=count]").val(count);
+		$("select[name=pitch]").val(pitch);
+		$("select[name=wpm]").val(wpm);
+		$("select[name=fwpm]").val(fwpm);
+	}
 });
